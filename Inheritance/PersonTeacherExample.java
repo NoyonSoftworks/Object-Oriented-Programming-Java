770947d9c2704f3d8faf69d1d788812de6992cc0
Noyon Hossain <113282502+NoyonSoftworks@users.noreply.github.com>
2025-10-24 14:40:19 +0600

    Update PersonTeacherExample.java

--- Inheritance/PersonTeacherExample.java
+++ Inheritance/PersonTeacherExample.java
@@ -1,1 +1,26 @@
-
+//declare person class
+class Person{
+    String name;
+    int age;
+    void callme1(){
+        System.out.println("Name: "+ name);
+        System.out.println("Age: "+ age);
+    }
+}
+//access person class data
+class Teacher extends Person{
+    String qualification;
+    void callme2(){
+        callme1(); //access person class data
+        System.out.println("Qualification: "+ qualification);
+    }
+}
+public class Main {
+public static void main(String[] args){
+    Teacher t1= new Teacher ();
+    t1.name="Noyon Hossain";
+    t1.age=22;
+    t1.qualification="Bsc In CSE";
+    t1.callme2(); ////access person class data
+}
+}
