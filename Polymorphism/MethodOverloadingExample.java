2b7a2f28fd642feb5d19f5cf0c1ede0f96374e55
Noyon Hossain <113282502+NoyonSoftworks@users.noreply.github.com>
2025-10-24 22:04:56 +0600

    Update MethodOverloadingExample.java

--- Polymorphism/MethodOverloadingExample.java
+++ Polymorphism/MethodOverloadingExample.java
@@ -1,1 +1,27 @@
-
+class overload{
+    // Two int parameters
+    void add(int a,int b){
+        System.out.println(a+b);
+    }
+    // Two float parameters
+    void add(float a,float b){
+        System.out.println(a+b);
+    }
+    // Three int parameters
+    void add(int a,int b,int c){
+        System.out.println(a+b+c);
+    }
+    // No parameters
+    void add(){
+        System.out.println("Nothing to add");
+    }
+}
+    public class Main {
+        public static void main(String[] args){
+            overload ol= new overload();
+            ol.add(); // No parameter
+            ol.add(5,10); // Two int
+            ol.add(5.5f,1.1f); // Two float (notice the f)
+            ol.add(5,10,20); // Three int
+        }
+    }
