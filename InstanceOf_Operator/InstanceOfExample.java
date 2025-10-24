16029d3fc992ff444ad235c17cdf401a5143d60f
Noyon Hossain <113282502+NoyonSoftworks@users.noreply.github.com>
2025-10-24 20:35:32 +0600

    Update InstanceOfExample.java

--- InstanceOf_Operator/InstanceOfExample.java
+++ InstanceOf_Operator/InstanceOfExample.java
@@ -1,1 +1,23 @@
+class Animal{
+    
+}
+class person extends Animal{
+    
+}
+class Teacher extends person{
+    
+}
 
+public class Main {
+public static void main(String[] args){
+    Animal a= new Animal();
+    person p= new person();
+    Teacher t= new Teacher();
+    
+    System.out.println(a instanceof Animal);
+    System.out.println(p instanceof Animal);
+    System.out.println(t instanceof person);
+    System.out.println(t instanceof Animal);
+    System.out.println(p instanceof Teacher);
+}
+}
