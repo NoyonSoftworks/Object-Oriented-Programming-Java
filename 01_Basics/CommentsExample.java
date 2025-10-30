28243e55ca6a36c91578f310a3e61faf84eb3dcf
Noyon Hossain <mdnoyon356268@gmail.com>
2025-10-30 21:43:30 +0600

    add new commentexample file in basics

---
+++ 01_Basics/CommentsExample.java
@@ -0,0 +1,21 @@
+// Java Program to Demonstrate Comments
+public class CommentsExample {
+    public static void main(String[] args) {
+
+        // This is a single-line comment
+        System.out.println("This line will be executed.");
+
+        /*
+         * This is a multi-line comment.
+         * It can span multiple lines and is ignored by the compiler.
+         */
+        System.out.println("This line will also be executed.");
+
+        /**
+         * This is a documentation comment.
+         * It is used to describe the purpose of a class or method.
+         * You can generate documentation using the javadoc tool.
+         */
+        System.out.println("Java supports 3 types of comments!");
+    }
+}
