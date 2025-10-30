3c24ef2abc8d2bfc800fad44fd404d7fef052a71
Noyon Hossain <mdnoyon356268@gmail.com>
2025-10-30 22:04:55 +0600

    add new variables files and solve name issue

---
+++ 01_Basics/VariablesAndDataTypes.java
@@ -0,0 +1,17 @@
+public class VariablesAndDataTypes {
+    public static void main(String[] args) {
+        //variables declaration
+        int age = 25; //integer variable
+        double cgpa = 3.88; //double variable
+        char blood = 'B'; //character variable
+        String name = "Noyon Hossain"; //String Variable
+        boolean isjavafun = true; //boolean variable
+        
+        //printing variables
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Blood Group: " + blood + " Positive");
+        System.out.println("Java Is Fun: " + isjavafun);
+    }    
+}
