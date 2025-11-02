a1c902dd5182801abf200c5a07c050dad958617f
Noyon Hossain <mdnoyon356268@gmail.com>
2025-11-02 14:12:52 +0600

    loops examples in java

---
+++ 01_Basics/LoopsExample.java
@@ -0,0 +1,32 @@
+public class LoopsExample {
+    //loop examples in java
+    public static void main(String[] args) {
+        //for loop
+        System.out.println("For Loop:");
+        for (int i=1; i<=5; i++) {
+            System.out.println(i);
+        }
+        //while loop
+        System.out.println("/nWhile Loop:");
+        int j=1;
+        while (j<=5) {
+            System.out.println(j);
+            j++;
+        }
+        //do while loop
+        System.out.println("/nDo While Loop:");
+        int k=1;
+        do {
+            System.out.println(k);
+            k++;
+        } while (k<=5);
+        //enhanced for loop
+        System.out.println("/nEnhanced For Loop:");
+        int[] numbers = {1, 2, 3, 4, 5};
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+
+    }
+    
+}
