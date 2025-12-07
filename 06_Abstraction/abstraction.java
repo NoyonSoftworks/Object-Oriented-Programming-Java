e56791ce2287f66a324e9ff2780826b777ae1e2e
Noyon Hossain <mdnoyon356268@gmail.com>
2025-12-08 01:25:38 +0600

    abstraction in java

--- 06_Abstraction/abstraction.java
+++ 06_Abstraction/abstraction.java
@@ -1,1 +1,22 @@
+abstract class Animal {
+    abstract void makeSound();
+    
+    void sleep() {
+        System.out.println("Zzz Zzz");
+    }
+}
 
+class Dog extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("Woof Woof");
+    }
+}
+
+class abstraction {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.makeSound(); // Output: Woof Woof
+        dog.sleep();     // Output: Zzz Zzz
+    }
+}
